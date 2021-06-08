620a5bf5c87951d605ff81a4cf28a5e8b71cbfa5
Landon Johnson <landonjohnsontechnologies@gmail.com>
2021-06-08 16:27:01 -0600

    🏺 whois.js update

--- lib/whois.ts
+++ lib/whois.ts
@@ -1,6 +1,7 @@
-//TODO: Fill out your application 
+//TODO: Fill out your application data inside this object
 
 export const whois = {
   domain: "ljtech.ca",
   handle: "ljtechdotca",
+  email: "help.ljtech@gmail.com",
 };
